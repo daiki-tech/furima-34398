14a41d8fd44ce45077ff25263a3de5a2af6734c4
daiki-tech <78465064+daiki-tech@users.noreply.github.com>
2021-03-15 14:28:20 +0900

    修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,19 @@
 class User < ApplicationRecord
 
-  has_many :items
-  has_one :buyer
+  #has_many :items
+  #has_one :buyer
 
+  #/[\p{katakana} ー－&&[^ -~｡-ﾟ]]+/
   VALID_PASSWORD_REGEX = /\A[a-z0-9]+\z/i
   validates :password, format: { with: VALID_PASSWORD_REGEX }
-  VALID_PASSWORD_REGEX = /\A[ぁ-んァ-ン一-龥]/
-  validates :family_name, :give_name, :family_name_kana, :give_name_kana, format: { with: VALID_PASSWORD_REGEX }
-  with_option presence: true do
+
+  VALID_NAME_REGEX = /\A[ぁ-んァ-ン一-龥々]+\z/
+  validates :family_name_kana, :give_name_kana, format: { with: VALID_NAME_REGEX }
+
+  VALID_KANA_REGEX = /\A[ァ-ヶー－]+\z/
+  validates :family_name_kana, :give_name_kana, format: { with: VALID_KANA_REGEX }
+
+  with_options presence: true do
     validates :nickname
     validates :family_name
     validates :give_name
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -30,30 +30,115 @@ RSpec.describe User, type: :model do
           @user.valid?
           expect(@user.errors.full_messages).to include "Email can't be blank"
         end
+        it '重複したemailの場合は登録できない' do
+          @user.email = ''
+          @user.valid?
+          expect(@user.errors.full_messages).to include "Duplicate emails cannot be registered"
+        end
+        it 'emailに@がない場合は登録できない' do
+          @user.email = 'hogehuga.com'
+          @user.valid?
+          expect(@user.errors.full_messages).to include "If there is no @ in the email, you cannot register"
+        end
         it 'passwordが空だと登録できない' do
           @user.password = ''
           @user.valid?
           expect(@user.errors.full_messages).to include "Password can't be blank"
+        end
+        it 'passwordが数値のみでは登録できない' do
+          @user.password = '123456'
+          @user.valid?
+          expect(@user.errors.full_messages).to include "Cannot register a password with only a numerical value"
+        end
+        it 'passwordが英字のみでは登録できない' do
+          @user.password = 'hogehoge'
+          @user.valid?
+          expect(@user.errors.full_messages).to include "You can't register a password with only English characters"
+        end
+        it 'passwordが全角では登録できない' do
+          @user.password = 'HOGE'
+          @user.valid?
+          expect(@user.errors.full_messages).to include "Cannot register a password with double-byte characters"
         end
         it 'family_nameが空だと登録できない' do
           @user.family_name = ''
           @user.valid?
           expect(@user.errors.full_messages).to include "Family name can't be blank"
         end
+        it 'family_nameが英語だと登録できない' do
+          @user.family_name = 'kana'
+          @user.valid?
+          expect(@user.errors.full_messages).to include "Cannot register if family name is in English"
+        end
+        it 'family_nameが数字だと登録できない' do
+          @user.family_name = '11'
+          @user.valid?
+          expect(@user.errors.full_messages).to include "Cannot register if family name is a number"
+        end
+        it 'family_nameが半角カタカナだと登録できない' do
+          @user.family_name = 'ｶﾅ'
+          @user.valid?
+          expect(@user.errors.full_messages).to include "Cannot register if family name is in half-width katakana"
+        end
         it 'give_nameが空だと登録できない' do
           @user.give_name = ''
           @user.valid?
           expect(@user.errors.full_messages).to include "Give name can't be blank"
+        end
+        it 'give_nameが英語だと登録できない' do
+          @user.give_name = 'kana'
+          @user.valid?
+          expect(@user.errors.full_messages).to include "Cannot register if give name is in English"
+        end
+        it 'give_nameが数字だと登録できない' do
+          @user.give_name = '11'
+          @user.valid?
+          expect(@user.errors.full_messages).to include "Cannot register if give name is a number"
+        end
+        it 'give_nameが半角カタカナだと登録できない' do
+          @user.give_name = 'ｶﾅ'
+          @user.valid?
+          expect(@user.errors.full_messages).to include "Cannot register if fgive name is in half-width katakana"
         end
         it 'family_name_kanaが空だと登録できない' do
           @user.family_name_kana = ''
           @user.valid?
           expect(@user.errors.full_messages).to include "Family name kana can't be blank"
         end
+        it 'family_name_kanaが数字だと登録できない' do
+          @user.family_name_kana = '11'
+          @user.valid?
+          expect(@user.errors.full_messages).to include "Cannot register if family name kana is a number"
+        end
+        it 'family_name_kanaが英語だと登録できない' do
+          @user.family_name_kana = 'kana'
+          @user.valid?
+          expect(@user.errors.full_messages).to include "Cannot register if family name kana is in English"
+        end
+        it 'family_name_kanaが半角カタカナだと登録できない' do
+          @user.family_name_kana = ''
+          @user.valid?
+          expect(@user.errors.full_messages).to include "Cannot register if family name kana is half-width katakana"
+        end
         it 'give_name_kanaが空だと登録できない' do
           @user.give_name_kana = ''
           @user.valid?
           expect(@user.errors.full_messages).to include "Give name kana can't be blank"
+        end
+        it 'give_name_kanaが数字だと登録できない' do
+          @user.family_name_kana = '11'
+          @user.valid?
+          expect(@user.errors.full_messages).to include "Cannot register if give name kana is a number"
+        end
+        it 'give_name_kanaが英語だと登録できない' do
+          @user.family_name_kana = 'kana'
+          @user.valid?
+          expect(@user.errors.full_messages).to include "Cannot register if give name kana is in English"
+        end
+        it 'give_name_kanaが半角カタカナだと登録できない' do
+          @user.family_name_kana = 'ｶﾅ'
+          @user.valid?
+          expect(@user.errors.full_messages).to include "Cannot register if give name kana is half-width katakana"
         end
         it 'birthdayが空だと登録できない' do
           @user.birthday = ''
