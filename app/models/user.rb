9f64c810b8644f5a0e8b14e8472b390603c2a2b0
daiki-tech <78465064+daiki-tech@users.noreply.github.com>
2021-03-15 14:46:15 +0900

    修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,8 +21,7 @@ class User < ApplicationRecord
     validates :give_name_kana
     validates :birthday
   end
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
