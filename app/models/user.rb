52adb18f644d95a1cfc7d7acf1c391effb2a76d0
daiki-tech <78465064+daiki-tech@users.noreply.github.com>
2021-03-22 14:45:35 +0900

    商品表示詳細機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,27 +1,15 @@
 class User < ApplicationRecord
-
-
-  #has_one :buyer
-
-  with_options presence: true do
-    validates :nickname
-    validates :birthday
-  end
-
+  has_many        :items
+  has_one         :buyer
   validates :password,format:{with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
-
   with_options format:  {with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/} do
     validates :family_name 
     validates :give_name
   end
-
   with_options format: {with: /\A[ァ-ヶー－]+\z/ } do
     validates :family_name_kana 
     validates :give_name_kana
   end
-
-  has_many :items
-  
   with_options presence: true do
     validates :nickname
     validates :family_name
@@ -30,7 +18,6 @@ class User < ApplicationRecord
     validates :give_name_kana
     validates :birthday
   end
-
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
-  resources :items, only:[:new, :create, :index]
+  resources :items, only:[:new, :create, :edit, :update, :destroy, :show, :index] do
+  end
+  resources :users, only:[:show]
 end
