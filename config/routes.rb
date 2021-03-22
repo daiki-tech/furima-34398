f79e0cdd3c3da68859a8aae916916acf81d18af0
daiki-tech <78465064+daiki-tech@users.noreply.github.com>
2021-03-22 16:08:25 +0900

    修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
-  resources :items, only:[:new, :create, :edit, :update, :destroy, :show, :index] do
+  resources :items do
   end
   resources :users, only:[:show]
 end
