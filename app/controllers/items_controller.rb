829c0bcc169222cd78b1f9e42ffabac5c7c86215
daiki-tech <78465064+daiki-tech@users.noreply.github.com>
2021-03-22 15:58:03 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -26,23 +26,23 @@ class ItemsController < ApplicationController
   def edit
   end
 
-  def update
-    @item = Item.find(params[:id])
-    if item.update(item_params)
-      redirect_to items_path
-    else
-      render :edit
-    end
-  end
+  # def update
+  #   @item = Item.find(params[:id])
+  #   if item.update(item_params)
+  #     redirect_to items_path
+  #   else
+  #     render :edit
+  #   end
+  # end
 
-  def destroy
-    @item = Item.find(params[:id])
-    if @item.destroy
-      redirect_to root_path
-    else
-      render :edit
-    end
-  end
+  # def destroy
+  #   @item = Item.find(params[:id])
+  #   if @item.destroy
+  #     redirect_to root_path
+  #   else
+  #     render :edit
+  #   end
+  # end
 
   private
 
